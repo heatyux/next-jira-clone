2f1159939041e0ab0b27bf08397b832acf86490d
heatyux <421743974@qq.com>
2025-07-01 15:59:30 +0800

    feat: customized shadcn button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl text-emerald-500">Hello, Next.js</h1>
+    <main className="space-y-2 space-x-2">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="tertiary">Tertiary</Button>
+      <Button disabled>Disabled</Button>
+
+      <hr />
+
+      <Button>Default</Button>
+      <Button size="icon">😊</Button>
+      <Button size="lg">Large</Button>
+      <Button size="sm">Small</Button>
+      <Button size="xs">Extra Small</Button>
     </main>
   )
 }
