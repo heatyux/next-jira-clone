e862b2f0d1ceadb128b864984f2fc48430fc3c26
heatyux <421743974@qq.com>
2025-07-04 10:37:13 +0800

    feat: handle login, register and loggout routes with appwrite

--- features/auth/server/route.ts
+++ features/auth/server/route.ts
@@ -1,22 +1,54 @@
 import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
+import { deleteCookie, setCookie } from 'hono/cookie'
+import { ID } from 'node-appwrite'
 
+import { createAdminClient } from '@/lib/appwrite'
+
+import { AUTH_COOKIE } from '../constants'
 import { signInFormSchema, signUpFormSchema } from '../schema'
 
 const app = new Hono()
   .post('/login', zValidator('json', signInFormSchema), async (ctx) => {
     const { email, password } = ctx.req.valid('json')
 
-    console.log({ email, password })
+    const { account } = await createAdminClient()
 
-    return ctx.json({ email, password })
+    const session = await account.createEmailPasswordSession(email, password)
+
+    setCookie(ctx, AUTH_COOKIE, session.secret, {
+      path: '/',
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 30, // 30 days
+    })
+
+    return ctx.json({ success: true })
   })
   .post('/register', zValidator('json', signUpFormSchema), async (ctx) => {
     const { name, email, password } = ctx.req.valid('json')
 
-    console.log({ name, email, password })
+    const { account } = await createAdminClient()
 
-    return ctx.json({ name, email, password })
+    await account.create(ID.unique(), email, password, name)
+
+    const session = await account.createEmailPasswordSession(email, password)
+
+    setCookie(ctx, AUTH_COOKIE, session.secret, {
+      path: '/',
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 30, // 30 days
+    })
+
+    return ctx.json({ success: true })
+  })
+  .post('/logout', async (ctx) => {
+    deleteCookie(ctx, AUTH_COOKIE)
+
+    return ctx.json({ success: true })
   })
 
 export default app
