cc78a80b48e55dbe0387bb5fe426c9df1fea795c
heatyux <421743974@qq.com>
2025-07-10 18:15:42 +0800

    feat: protected workspace page

--- app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,4 +1,14 @@
-const WorkspaceIdPage = () => {
+import { redirect } from 'next/navigation'
+
+import { getCurrent } from '@/features/auth/actions'
+
+const WorkspaceIdPage = async () => {
+  const user = await getCurrent()
+
+  if (!user) {
+    redirect('/sign-in')
+  }
+
   return <>Workspace Id Page</>
 }
 
